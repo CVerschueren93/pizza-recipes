e12d26c3960f6d6110e998d79717f9f55ed0cbe8
Chantal Verschueren <cverschueren34@gmail.com>
2023-10-24 16:46:52 +0200

    Delete & Update files

---
+++ src/RecipeListPage.jsx
@@ -0,0 +1,10 @@
+import "./RecipeListPage.css";
+
+function RecipeListPage() {
+  return (
+    <div className="App">
+      <h1>Pizza Recipes</h1>
+    </div>
+  );
+}
+export default RecipeListPage;
